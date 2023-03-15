e3afb7d7e4c3d88e8941143e34dac3f7d6b42922
JuanCarlosHernandezLomas <hernandez.lomas.juan102@gmail.com>
2023-03-15 15:08:29 -0600

    implementar avatar

--- components/Screen/HomeUser.js
+++ components/Screen/HomeUser.js
@@ -1,6 +1,6 @@
-import { View,Text } from "native-base";
+import { View,Text, Center} from "native-base";
 import React from "react";
-
+import { Avatar } from 'react-native-elements';
 
 const HomeUser=()=>{
     return(
@@ -12,7 +12,18 @@ const HomeUser=()=>{
                 textAlign:"center",
                 marginTop: "20%"
             }}>
-            User</Text>
+            Perfil</Text>
+            <Center>
+            <Avatar
+            size={"xlarge"}
+  rounded
+  source={{
+    uri:
+      'https://previews.123rf.com/images/rawpixel/rawpixel1704/rawpixel170441704/76561515-retrato-de-personas-estudio-disparar-con-expresi%C3%B3n-de-cara-sonriente.jpg',
+  }}
+/>
+</Center>
+
         </View>        
     );
 }
